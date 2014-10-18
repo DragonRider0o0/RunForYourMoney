7aa2d79e79542fa1a20c9233c22fb3e0f432d71c
DragonRider0o0 <DragonRider0o0@gmail.com>
2014-10-18 19:44:50 -0400

    Added Settings screen and all other menus

--- src/main.c
+++ src/main.c
@@ -16,7 +16,7 @@ typedef enum {
 	   STATS = 8,
 	   PAST_RACE = 9
 } screen;
-
+static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
 	
 	//Splash Elements
 	static GBitmap *splash_bitmap;
@@ -26,13 +26,29 @@ typedef enum {
 #define NUM_HOME_MENU_ITEMS  5
 static SimpleMenuLayer* home_menu_layer;
 static SimpleMenuItem home_menu_items[NUM_HOME_MENU_ITEMS];
-static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
+
 
 // Active Elements
 #define NUM_ACTIVE_MENU_ITEMS  4
 static SimpleMenuLayer* active_menu_layer;
 static SimpleMenuItem active_menu_items[NUM_ACTIVE_MENU_ITEMS];
-static SimpleMenuSection active_sections[NUM_MENU_SECTIONS];
+
+
+// New Elements
+#define NUM_NEW_MENU_ITEMS  2
+static SimpleMenuLayer* new_menu_layer;
+static SimpleMenuItem new_menu_items[NUM_NEW_MENU_ITEMS];
+
+
+// Old Elements
+#define NUM_OLD_MENU_ITEMS  4
+static SimpleMenuLayer* old_menu_layer;
+static SimpleMenuItem old_menu_items[NUM_OLD_MENU_ITEMS];
+
+// Settings Elements
+#define NUM_SETTINGS_MENU_ITEMS  3
+static SimpleMenuLayer* settings_menu_layer;
+static SimpleMenuItem settings_menu_items[NUM_SETTINGS_MENU_ITEMS];
 
 
 ////////////////////////////////////////////////////////////////////////////
@@ -101,6 +117,7 @@ void home_window(Window *window)
   
   menu_sections[0] = (SimpleMenuSection)
 		{
+			 .title = "RunForYourMoney",
     .num_items = NUM_HOME_MENU_ITEMS,
     .items = home_menu_items,
   };
@@ -176,6 +193,156 @@ void active_window(Window *window)
 ////////////////////////////////////////////////////////////////////////////
 
 
+void new_menu_select_callback(int index, void *ctx) 
+{
+  new_menu_items[index].subtitle = "You've hit select here!";
+  layer_mark_dirty(simple_menu_layer_get_layer(new_menu_layer));
+}
+
+void new_window(Window *window)
+{
+	 tiny_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TINY);
+ 	window_set_status_bar_icon	(window, tiny_bitmap);
+	
+	int num_a_items = 0;
+  new_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Join Challenge",
+    .callback = new_menu_select_callback
+  };
+  new_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Start Challenge",
+    .callback = new_menu_select_callback
+  };
+  
+  menu_sections[0] = (SimpleMenuSection)
+		{
+    .num_items = NUM_NEW_MENU_ITEMS,
+    .items = new_menu_items,
+  };
+	
+	 Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  // Initialize the simple menu layer
+  new_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS, NULL);
+
+  // Add it to the window for display
+  layer_add_child(window_layer, simple_menu_layer_get_layer(new_menu_layer));
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
+void old_menu_select_callback(int index, void *ctx) 
+{
+  old_menu_items[index].subtitle = "You've hit select here!";
+  layer_mark_dirty(simple_menu_layer_get_layer(old_menu_layer));
+}
+
+void old_window(Window *window)
+{
+	 tiny_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TINY);
+ 	window_set_status_bar_icon	(window, tiny_bitmap);
+	
+	int num_a_items = 0;
+  old_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Office Race",
+    .callback = old_menu_select_callback
+  };
+  old_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "New Race",
+    .callback = old_menu_select_callback
+  };
+  old_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Family",
+    .callback = old_menu_select_callback
+  };
+	  old_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "Friends",
+    .callback = old_menu_select_callback
+  };
+  
+  menu_sections[0] = (SimpleMenuSection)
+		{
+    .num_items = NUM_OLD_MENU_ITEMS,
+    .items = old_menu_items,
+  };
+	
+	 Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  // Initialize the simple menu layer
+  old_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS, NULL);
+
+  // Add it to the window for display
+  layer_add_child(window_layer, simple_menu_layer_get_layer(old_menu_layer));
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
+void settings_menu_select_callback(int index, void *ctx) 
+{
+  settings_menu_items[index].subtitle = "You've hit select here!";
+  layer_mark_dirty(simple_menu_layer_get_layer(settings_menu_layer));
+}
+
+void settings_window(Window *window)
+{
+	 tiny_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TINY);
+ 	window_set_status_bar_icon	(window, tiny_bitmap);
+	
+	int num_a_items = 0;
+  settings_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Vibration",
+    .callback = settings_menu_select_callback
+  };
+  settings_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Invert Colors",
+    .callback = settings_menu_select_callback
+  };
+  settings_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Units",
+    .callback = settings_menu_select_callback
+  };
+  
+  menu_sections[0] = (SimpleMenuSection)
+		{
+    .num_items = NUM_SETTINGS_MENU_ITEMS,
+    .items = settings_menu_items,
+  };
+	
+	 Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  // Initialize the simple menu layer
+  settings_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS, NULL);
+
+  // Add it to the window for display
+  layer_add_child(window_layer, simple_menu_layer_get_layer(settings_menu_layer));
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
 void drawWindow(screen target, Window *window)
 {
 			switch(target)
@@ -183,9 +350,9 @@ void drawWindow(screen target, Window *window)
     case SPLASH: splash_window(window); return;
     case HOME: home_window(window); return;
     case ACTIVE:active_window(window); return;
-				case NEW: printf("pos inf"); return;
-				case OLD: printf("pos inf"); return;
-				case SETTINGS: printf("pos inf"); return;
+				case NEW: new_window(window); return;
+				case OLD: old_window(window); return;
+				case SETTINGS: settings_window(window); return;
 				case START: printf("pos inf"); return;
 				case RACE: printf("pos inf"); return;
 				case STATS: printf("pos inf"); return;
@@ -203,8 +370,12 @@ void drawWindow(screen target, Window *window)
 void window_load(Window *window)
 {
 	  //drawWindow(SPLASH, window);
-   //drawWindow(HOME, window);
-	  drawWindow(ACTIVE, window);
+   drawWindow(HOME, window);
+	  //drawWindow(ACTIVE, window);
+	  //drawWindow(NEW, window);
+	  //drawWindow(OLD, window);
+	  //drawWindow(SETTINGS, window);
+	  
   
 }
  
