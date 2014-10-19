a5a841cb947043499e90bb372cc4833b372440cf
DragonRider0o0 <DragonRider0o0@gmail.com>
2014-10-18 22:10:18 -0400

    Attempt Star Badge

--- src/main.c
+++ src/main.c
@@ -14,7 +14,7 @@ typedef enum {
 	   START = 6,
 	   RACE = 7,
 	   STATS = 8,
-	   PAST_RACE = 9
+	   REVIEW = 9
 } screen;
 static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
 	
@@ -45,16 +45,35 @@ static SimpleMenuItem new_menu_items[NUM_NEW_MENU_ITEMS];
 static SimpleMenuLayer* past_menu_layer;
 static SimpleMenuItem past_menu_items[NUM_PAST_MENU_ITEMS];
 
+
 // Settings Elements
 #define NUM_SETTINGS_MENU_ITEMS  3
 static SimpleMenuLayer* settings_menu_layer;
 static SimpleMenuItem settings_menu_items[NUM_SETTINGS_MENU_ITEMS];
 
 
+// Stats Elements
+#define NUM_STATS_MENU_ITEMS  5
+static SimpleMenuLayer* stats_menu_layer;
+static SimpleMenuItem stats_menu_items[NUM_STATS_MENU_ITEMS];
+
+
+// Review Elements
+#define NUM_REVIEW_MENU_ITEMS  5
+static SimpleMenuLayer* review_menu_layer;
+static SimpleMenuItem review_menu_items[NUM_REVIEW_MENU_ITEMS];
+static GBitmap *star_image;
+
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
 
+
+//void splash_select_callback(int index, void *ctx) 
+//{
+//  home_menu_items[index].subtitle = "You've hit select here!";
+//  layer_mark_dirty(simple_menu_layer_get_layer(home_menu_layer));
+//}
 
 void splash_window(Window *window)
 	{
@@ -67,6 +86,7 @@ void splash_window(Window *window)
 	 
 	 splash_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
   bitmap_layer_set_bitmap(splash_layer, splash_bitmap);
+	 //splash_layer.callback = splash_callback;
   layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(splash_layer));
 }
 
@@ -95,7 +115,7 @@ void home_window(Window *window)
   };
   home_menu_items[num_a_items++] = (SimpleMenuItem)
 		{
-    .title = "New Challenges",
+    .title = "New Challenge",
     .callback = home_menu_select_callback
   };
 	  home_menu_items[num_a_items++] = (SimpleMenuItem)
@@ -342,6 +362,132 @@ void settings_window(Window *window)
 ////////////////////////////////////////////////////////////////////////////
 
 
+void stats_menu_select_callback(int index, void *ctx) 
+{
+  stats_menu_items[index].title = "End Run";
+  layer_mark_dirty(simple_menu_layer_get_layer(stats_menu_layer));
+}
+
+void stats_window(Window *window)
+{
+	 tiny_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TINY);
+ 	window_set_status_bar_icon	(window, tiny_bitmap);
+	
+	int num_a_items = 0;
+  stats_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Time Elapsed",
+			.subtitle = "00:32:21"
+		};
+  stats_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Pace",
+			.subtitle = "11:21 min/mi"
+  };
+  stats_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Distance",
+			 .subtitle = "2.99 mi"
+  };
+	  stats_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "Challenge",
+				.subtitle = "By The New Year"
+  };
+		  stats_menu_items[num_a_items++] = (SimpleMenuItem)
+				{
+    .title = "Pause",
+    .callback = stats_menu_select_callback
+  };
+
+  
+  menu_sections[0] = (SimpleMenuSection)
+		{
+			.title = "Run Stats",
+    .num_items = NUM_STATS_MENU_ITEMS,
+    .items = stats_menu_items
+  };
+	
+	 Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  // Initialize the simple menu layer
+  stats_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS, NULL);
+
+  // Add it to the window for display
+  layer_add_child(window_layer, simple_menu_layer_get_layer(stats_menu_layer));
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
+void review_menu_select_callback(int index, void *ctx) 
+{
+  review_menu_items[index].subtitle = "You've hit select here!";
+  layer_mark_dirty(simple_menu_layer_get_layer(review_menu_layer));
+}
+
+void review_window(Window *window)
+{
+	 tiny_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TINY);
+ 	window_set_status_bar_icon	(window, tiny_bitmap);
+	
+	 star_image = gbitmap_create_with_resource(RESOURCE_ID_VICTORY);
+	int num_a_items = 0;
+  review_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Challenge Type:",
+			.subtitle = "Speed"
+  };
+  review_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "Paid",
+    .subtitle = "April 20, 2014"
+  };
+  review_menu_items[num_a_items++] = (SimpleMenuItem)
+		{
+    .title = "1. Jeff Shaw",
+    .subtitle = "Finished in 4h 02m. Won $60",
+				.icon = star_image
+  };
+	  review_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "2. Megan Holtzblat",
+    .subtitle = "Finished in 5h 08m. Won $0"
+  };
+		  review_menu_items[num_a_items++] = (SimpleMenuItem)
+				{
+    .title = "3. Angelo Caballo (You)",
+    .subtitle = "Finished in 5h 45m. Won $0"
+  };
+
+  
+  menu_sections[0] = (SimpleMenuSection)
+		{
+			.title = "Easter Marathon",
+    .num_items = NUM_REVIEW_MENU_ITEMS,
+    .items = review_menu_items,
+  };
+	
+	 Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  // Initialize the simple menu layer
+  review_menu_layer = simple_menu_layer_create(bounds, window, menu_sections, NUM_MENU_SECTIONS, NULL);
+
+  // Add it to the window for display
+  layer_add_child(window_layer, simple_menu_layer_get_layer(review_menu_layer));
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
 void drawWindow(screen target, Window *window)
 {
 			switch(target)
@@ -354,8 +500,8 @@ void drawWindow(screen target, Window *window)
 				case SETTINGS: settings_window(window); return;
 				case START: printf("pos inf"); return;
 				case RACE: printf("pos inf"); return;
-				case STATS: printf("pos inf"); return;
-				case PAST_RACE: printf("pos inf"); return;
+				case STATS: stats_window(window); return;
+				case REVIEW: review_window(window); return;
     default: printf("not special"); break;
    }	
 }
@@ -370,12 +516,12 @@ void window_load(Window *window)
 {
 	  //drawWindow(SPLASH, window);
    //drawWindow(HOME, window);
-	  drawWindow(ACTIVE, window);
+	  //drawWindow(ACTIVE, window);
 	  //drawWindow(NEW, window);
 	  //drawWindow(PAST, window);
 	  //drawWindow(SETTINGS, window);
-	  
-  
+	  //drawWindow(STATS, window);
+	  drawWindow(REVIEW, window);
 }
  
 void window_unload(Window *window)
@@ -391,6 +537,8 @@ void window_unload(Window *window)
 	simple_menu_layer_destroy(new_menu_layer);
 	simple_menu_layer_destroy(past_menu_layer);
 	simple_menu_layer_destroy(settings_menu_layer);
+	simple_menu_layer_destroy(stats_menu_layer);
+	simple_menu_layer_destroy(review_menu_layer);
 }
  
 void init()
