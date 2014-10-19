580a1ccf221632489b22947166577af65fa32db6
DragonRider0o0 <DragonRider0o0@gmail.com>
2014-10-19 11:00:41 -0400

    Final version with back

--- src/main.c
+++ src/main.c
@@ -20,46 +20,47 @@ typedef enum {
 
 static screen origin_screen;
 static screen target_screen;
-	
+	static int ticks;
 static SimpleMenuSection menu_sections[NUM_MENU_SECTIONS];
 
 void click_config_provider(void *context);
 void select_config_provider(void *context);
 void back_config_provider(void *context);
 void drawScreen(screen target_screen);
-void tick_handler(struct tm *tick_time, TimeUnits units_changed);
+void flag_handler(struct tm *tick_time, TimeUnits units_changed);
+void run_handler(struct tm *tick_time, TimeUnits units_changed);
 	
 	//Splash Elements
 	static GBitmap *splash_bitmap;
 	static BitmapLayer *splash_layer;
 	
 // Home Elements
-#define NUM_HOME_MENU_ITEMS  4
+#define NUM_HOME_MENU_ITEMS  5
 static SimpleMenuLayer* home_menu_layer;
 static SimpleMenuItem home_menu_items[NUM_HOME_MENU_ITEMS];
 
 
 
 // Active Elements
-#define NUM_ACTIVE_MENU_ITEMS  4
+#define NUM_ACTIVE_MENU_ITEMS  5
 static SimpleMenuLayer* active_menu_layer;
 static SimpleMenuItem active_menu_items[NUM_ACTIVE_MENU_ITEMS];
 
 
 // New Elements
-#define NUM_NEW_MENU_ITEMS  2
+#define NUM_NEW_MENU_ITEMS  3
 static SimpleMenuLayer* new_menu_layer;
 static SimpleMenuItem new_menu_items[NUM_NEW_MENU_ITEMS];
 
 
 // Past Elements
-#define NUM_PAST_MENU_ITEMS  4
+#define NUM_PAST_MENU_ITEMS  5
 static SimpleMenuLayer* past_menu_layer;
 static SimpleMenuItem past_menu_items[NUM_PAST_MENU_ITEMS];
 
 
 // Settings Elements
-#define NUM_SETTINGS_MENU_ITEMS  3
+#define NUM_SETTINGS_MENU_ITEMS  4
 static SimpleMenuLayer* settings_menu_layer;
 static SimpleMenuItem settings_menu_items[NUM_SETTINGS_MENU_ITEMS];
 
@@ -81,17 +82,33 @@ static SimpleMenuItem settings_menu_items[NUM_SETTINGS_MENU_ITEMS];
 	static BitmapLayer *flag_6_layer;
 	static GBitmap *flag_7_bitmap;
 	static BitmapLayer *flag_7_layer;
-static int ticks;
+
+
+// Race Elements
+	static GBitmap *run_1_bitmap;
+	static BitmapLayer *run_1_layer;
+	static GBitmap *run_2_bitmap;
+	static BitmapLayer *run_2_layer;
+	static GBitmap *run_3_bitmap;
+	static BitmapLayer *run_3_layer;
+	static GBitmap *run_4_bitmap;
+	static BitmapLayer *run_4_layer;
+	static GBitmap *run_4_bitmap;
+	static BitmapLayer *run_4_layer;
+	static GBitmap *run_5_bitmap;
+	static BitmapLayer *run_5_layer;
+	static GBitmap *run_6_bitmap;
+	static BitmapLayer *run_6_layer;
 
 
 // Stats Elements
-#define NUM_STATS_MENU_ITEMS  5
+#define NUM_STATS_MENU_ITEMS  6
 static SimpleMenuLayer* stats_menu_layer;
 static SimpleMenuItem stats_menu_items[NUM_STATS_MENU_ITEMS];
 
 
 // Review Elements
-#define NUM_REVIEW_MENU_ITEMS  5
+#define NUM_REVIEW_MENU_ITEMS  6
 static SimpleMenuLayer* review_menu_layer;
 static SimpleMenuItem review_menu_items[NUM_REVIEW_MENU_ITEMS];
 static GBitmap *star_image;
@@ -132,6 +149,7 @@ void home_menu_select_callback(int index, void *ctx)
 			 case 1: target_screen = NEW; origin_screen = HOME; break;
 			 case 2: target_screen = PAST; origin_screen = HOME; break;
 			 case 3: target_screen = SETTINGS; origin_screen = HOME; break;
+			 case 4: target_screen = SPLASH; origin_screen = HOME; break;
 			 default: break;
 		};
   drawScreen(target_screen);
@@ -164,6 +182,11 @@ void home_window()
     .title = "Settings",
     .callback = home_menu_select_callback
   };
+			  home_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "Back",
+    .callback = home_menu_select_callback
+  };
 
   
   menu_sections[0] = (SimpleMenuSection)
@@ -193,10 +216,11 @@ void active_menu_select_callback(int index, void *ctx)
 {
    	switch(index)
 		{
-				case 0: target_screen = REVIEW; 	origin_screen = ACTIVE; break;
-			 case 1: target_screen = REVIEW; 	origin_screen = ACTIVE; break;
-			 case 2: target_screen = REVIEW; 	origin_screen = ACTIVE; break;
-			 case 3: target_screen = REVIEW; 	origin_screen = ACTIVE; break;
+				case 0: target_screen = START; 	origin_screen = ACTIVE; break;
+			 case 1: target_screen = START; 	origin_screen = ACTIVE; break;
+			 case 2: target_screen = START; 	origin_screen = ACTIVE; break;
+			 case 3: target_screen = START; 	origin_screen = ACTIVE; break;
+					case 4: target_screen = SPLASH; origin_screen = ACTIVE; break;
 			 default: break;
 		};
   drawScreen(target_screen);
@@ -228,6 +252,11 @@ void active_window()
     .title = "Friends in Italy",
     .callback = active_menu_select_callback
   };
+		  active_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "Back",
+    .callback = active_menu_select_callback
+  };
   
   menu_sections[0] = (SimpleMenuSection)
 		{
@@ -256,8 +285,9 @@ void new_menu_select_callback(int index, void *ctx)
 {
   switch(index)
 		{
-				case 0: target_screen = STATS;	origin_screen = NEW; break;
-			 case 1: target_screen = STATS; origin_screen = NEW; break;
+				case 0: target_screen = START;	origin_screen = NEW; break;
+			 case 1: target_screen = START; origin_screen = NEW; break;
+			case 2: target_screen = SPLASH; origin_screen = NEW; break;
 			 default: break;
 		};
   drawScreen(target_screen);
@@ -277,6 +307,11 @@ void new_window()
   new_menu_items[num_a_items++] = (SimpleMenuItem)
 		{
     .title = "Start Challenge",
+    .callback = new_menu_select_callback
+  };
+			  new_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "Back",
     .callback = new_menu_select_callback
   };
   
@@ -311,6 +346,7 @@ void past_menu_select_callback(int index, void *ctx)
 			 case 1: target_screen = REVIEW; origin_screen = PAST; break;
 			 case 2: target_screen = REVIEW; origin_screen = PAST; break;
 			 case 3: target_screen = REVIEW; origin_screen = PAST; break;
+			 case 4: target_screen = SPLASH; origin_screen = PAST; break;
 			 default: break;
 		};
   drawScreen(target_screen);
@@ -340,6 +376,11 @@ void past_window()
 	  past_menu_items[num_a_items++] = (SimpleMenuItem)
 			{
     .title = "Friends",
+    .callback = past_menu_select_callback
+  };
+			  past_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "Back",
     .callback = past_menu_select_callback
   };
   
@@ -373,6 +414,7 @@ void settings_menu_select_callback(int index, void *ctx)
 				case 0: target_screen = SETTINGS; vibes_long_pulse(); break;
 			 case 1: target_screen = SETTINGS; break;
 			 case 2: target_screen = SETTINGS; break;
+		 	case 3: target_screen = SPLASH; origin_screen = SETTINGS; break;
 			 default: break;
 		};
   drawScreen(target_screen);
@@ -397,6 +439,11 @@ void settings_window()
   settings_menu_items[num_a_items++] = (SimpleMenuItem)
 		{
     .title = "Units",
+    .callback = settings_menu_select_callback
+  };
+			  settings_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "Back",
     .callback = settings_menu_select_callback
   };
   
@@ -425,8 +472,7 @@ void settings_window()
 void start_window()
 	{
 	ticks = 0;
-	 //origin_screen = PEBBLE;
-	 target_screen =  STATS;
+	 target_screen =  RACE;
 	 tiny_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TINY);
  	window_set_status_bar_icon	(window, tiny_bitmap);
 	
@@ -455,7 +501,47 @@ void start_window()
 	bitmap_layer_set_bitmap(flag_6_layer, flag_6_bitmap);
 	bitmap_layer_set_bitmap(flag_7_layer, flag_7_bitmap);
 	 //splash_layer.callback = splash_callback;
-  tick_timer_service_subscribe(SECOND_UNIT, (TickHandler) tick_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, (TickHandler) flag_handler);
+	
+}
+
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
+void race_window()
+	{
+	ticks = 0;
+	 target_screen =  STATS;
+	 tiny_bitmap = gbitmap_create_with_resource(RESOURCE_ID_TINY);
+ 	window_set_status_bar_icon	(window, tiny_bitmap);
+	
+	run_1_bitmap= gbitmap_create_with_resource(RESOURCE_ID_RUN_1);
+	run_2_bitmap= gbitmap_create_with_resource(RESOURCE_ID_RUN_2);
+	run_3_bitmap= gbitmap_create_with_resource(RESOURCE_ID_RUN_3);
+	run_4_bitmap= gbitmap_create_with_resource(RESOURCE_ID_RUN_4);
+	run_5_bitmap= gbitmap_create_with_resource(RESOURCE_ID_RUN_5);
+	run_6_bitmap= gbitmap_create_with_resource(RESOURCE_ID_RUN_6);
+	 
+	run_1_layer = bitmap_layer_create(GRect(20, 80, 40, 40));
+	run_2_layer = bitmap_layer_create(GRect(20, 80, 40, 40));
+	run_3_layer = bitmap_layer_create(GRect(20, 80, 40, 40));
+	run_4_layer = bitmap_layer_create(GRect(20, 80, 40, 40));
+	run_5_layer = bitmap_layer_create(GRect(20, 80, 40, 40));
+	run_6_layer = bitmap_layer_create(GRect(20, 80, 40, 40));
+
+	
+  bitmap_layer_set_bitmap(run_1_layer, run_1_bitmap);
+	bitmap_layer_set_bitmap(run_2_layer, run_2_bitmap);
+	bitmap_layer_set_bitmap(run_3_layer, run_3_bitmap);
+	bitmap_layer_set_bitmap(run_4_layer, run_4_bitmap);
+	bitmap_layer_set_bitmap(run_5_layer, run_5_bitmap);
+	bitmap_layer_set_bitmap(run_6_layer, run_6_bitmap);
+
+	 //splash_layer.callback = splash_callback;
+  tick_timer_service_subscribe(SECOND_UNIT, (TickHandler) run_handler);
 	
 }
 
@@ -473,7 +559,20 @@ void stats_menu_select_callback(int index, void *ctx)
 			 case 1: target_screen = STATS; break;
 			 case 2: target_screen = STATS; break;
 				case 3: target_screen = STATS; break;
-				case 4: target_screen = STATS; stats_menu_items[index].title = "End Run"; layer_mark_dirty(simple_menu_layer_get_layer(stats_menu_layer)); return;
+				case 4: 
+				{
+					if(strcmp(stats_menu_items[index].title,"Pause"))
+					{
+						stats_menu_items[index].title = "End Run"; 
+						layer_mark_dirty(simple_menu_layer_get_layer(stats_menu_layer));
+					} 
+					else
+					{
+						target_screen = SPLASH; 
+					} return;
+				}
+				case 5: target_screen = SPLASH; origin_screen = STATS; 
+					break;
 			 default: break;
 		};
   drawScreen(target_screen);
@@ -510,6 +609,11 @@ void stats_window()
     .title = "Pause",
     .callback = stats_menu_select_callback
   };
+			  stats_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "Back",
+    .callback = stats_menu_select_callback
+  };
 
   
   menu_sections[0] = (SimpleMenuSection)
@@ -544,6 +648,7 @@ void review_menu_select_callback(int index, void *ctx)
 			 case 2: target_screen = REVIEW; break;
 		 	case 3: target_screen = REVIEW; break;
 			 case 4: target_screen = REVIEW; break;
+			 case 5: target_screen = SPLASH; origin_screen = REVIEW; break;
 			 default: break;
 		};
   drawScreen(target_screen);
@@ -581,6 +686,11 @@ void review_window()
 				{
     .title = "3. Angelo Caballo",
     .subtitle = "Time: 5h 45m"
+  };
+			  review_menu_items[num_a_items++] = (SimpleMenuItem)
+			{
+    .title = "Back",
+    .callback = review_menu_select_callback
   };
 
   
@@ -636,6 +746,12 @@ void window_unload(Window *window)
 	bitmap_layer_destroy(flag_5_layer);
 	bitmap_layer_destroy(flag_6_layer);
 	bitmap_layer_destroy(flag_7_layer);
+		bitmap_layer_destroy(run_1_layer);
+	bitmap_layer_destroy(run_2_layer);
+	bitmap_layer_destroy(run_3_layer);
+	bitmap_layer_destroy(run_4_layer);
+	bitmap_layer_destroy(run_5_layer);
+	bitmap_layer_destroy(run_6_layer);
 	
 	 simple_menu_layer_destroy(home_menu_layer);
 	 simple_menu_layer_destroy(active_menu_layer);
@@ -671,6 +787,17 @@ void reset_window()
 	flag_6_layer = NULL;
 	bitmap_layer_destroy(flag_7_layer);
 	flag_7_layer = NULL;
+		run_1_layer = NULL;
+	bitmap_layer_destroy(run_2_layer);
+	run_2_layer = NULL;
+	bitmap_layer_destroy(run_3_layer);
+	run_3_layer = NULL;
+	bitmap_layer_destroy(run_4_layer);
+	run_4_layer = NULL;
+	bitmap_layer_destroy(run_5_layer);
+	run_5_layer = NULL;
+	bitmap_layer_destroy(run_6_layer);
+	run_6_layer = NULL;
 	
 	 simple_menu_layer_destroy(home_menu_layer);
 	home_menu_layer = NULL;
@@ -706,7 +833,7 @@ void drawScreen(screen target)
 				case PAST: past_window(); break;
 				case SETTINGS: settings_window(); break;
 				case START: start_window(); break;
-				case RACE: printf("pos inf"); break;
+				case RACE: race_window(); break;
 				case STATS: stats_window(); break;
 				case REVIEW: review_window(); break;
 				case PEBBLE:  window_stack_pop(true);
@@ -722,12 +849,14 @@ void drawScreen(screen target)
  
 void select_click_handler(ClickRecognizerRef recognizer, void *context)
 {
+	vibes_short_pulse();
 	drawScreen(target_screen);
 }
 
 void back_click_handler(ClickRecognizerRef recognizer, void *context)
 {
 	//vibes_long_pulse();
+	vibes_short_pulse();
 	drawScreen(origin_screen);
 }
 
@@ -749,7 +878,12 @@ void back_config_provider(void *context)
 }
 
 
-void tick_handler(struct tm *tick_time, TimeUnits units_changed)
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
+void flag_handler(struct tm *tick_time, TimeUnits units_changed)
 {
 				switch(ticks)
 					{
@@ -760,10 +894,27 @@ void tick_handler(struct tm *tick_time, TimeUnits units_changed)
 					 case 4:  layer_remove_child_layers(window_get_root_layer(window)); layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(flag_5_layer));	break;
 					 case 5:  layer_remove_child_layers(window_get_root_layer(window)); layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(flag_6_layer));	break;
 					 case 6:  layer_remove_child_layers(window_get_root_layer(window)); layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(flag_7_layer)); vibes_long_pulse();	break;
-      default: break;
+      default: layer_remove_child_layers(window_get_root_layer(window)); tick_timer_service_unsubscribe(); origin_screen = START;  drawScreen(RACE);	 break;
 				}
 	ticks = ticks + 1;
 }
+
+
+void run_handler(struct tm *tick_time, TimeUnits units_changed)
+{
+				switch(ticks)
+					{
+						case 0:  layer_remove_child_layers(window_get_root_layer(window)); layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(run_1_layer));	break;
+					 case 1:  layer_remove_child_layers(window_get_root_layer(window)); layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(run_2_layer));	break;
+					 case 2:  layer_remove_child_layers(window_get_root_layer(window)); layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(run_3_layer));	break;
+					 case 3:  layer_remove_child_layers(window_get_root_layer(window)); layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(run_4_layer)); break;
+					 case 4:  layer_remove_child_layers(window_get_root_layer(window)); layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(run_5_layer));	break;
+					 case 5:  layer_remove_child_layers(window_get_root_layer(window)); layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(run_6_layer));	break;
+      default: ticks = 0; return;
+				}
+	ticks = ticks + 1;
+}
+
 
 ////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
@@ -778,7 +929,7 @@ void init()
     .unload = window_unload,
   });
 	
-	drawScreen(START);
+	drawScreen(SPLASH);
 	window_set_click_config_provider(window, click_config_provider);
 	
 	window_stack_push(window, true);
